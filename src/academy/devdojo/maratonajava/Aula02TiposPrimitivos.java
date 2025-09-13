2c1a91849bc89820437e4e1e587c3ec6b09dd0eb
eduardo.fabri <eduardo.fabri@wises.com.br>
2025-09-12 23:31:49 -0300

    Video 13 - Casting

--- src/academy/devdojo/maratonajava/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/Aula02TiposPrimitivos.java
@@ -4,13 +4,16 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long e boolean
 
-        int age = 16;
-        double salarioDouble = 20000;
-        float salarioFloat = 20.3f;
+        int age = (int) 16000000000L;
+        double salarioDouble = 200000;
+        float salarioFloat = (float) 2500.3D;
         char caractere = 'a';
         byte ageByte = 10;
         short ageShort = 10;
         long salarioLong = 100000;
         boolean isVerdadeiro = true;
+
+        System.out.println(age);
+        System.out.println(salarioFloat);
     }
 }
