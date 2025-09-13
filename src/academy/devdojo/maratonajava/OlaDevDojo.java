de32337719a0057d4ac51b8115689a322dd6d838
eduardo.fabri <eduardo.fabri@wises.com.br>
2025-09-12 22:52:33 -0300

    Video 09 - Comentarios

---
+++ src/academy/devdojo/maratonajava/OlaDevDojo.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava;
+
+public class OlaDevDojo {
+    public static void main(String[] args) {
+        // Comentario de uma linha
+        /* Comentario de multiplas
+            linhas
+        */
+
+        System.out.println("Ola DevDojo :)");
+    }
+}
