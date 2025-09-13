cfca7673e31af1f4a17920dee18c2daa07d3bc29
eduardo.fabri <eduardo.fabri@wises.com.br>
2025-09-13 12:58:56 -0300

    Video 17 e 18 - Operadores relacionais e condicionais

--- src/academy/devdojo/maratonajava/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/Aula04Operadores.java
@@ -27,5 +27,23 @@ public class Aula04Operadores {
         System.out.println("Dez igual a vinte: " + isDezIgualAVinte);
         System.out.println("Dez igual a dez: " + isDezIgualDez);
         System.out.println("Dez diferente de vinte: " + isDezDiferenteDeVinte);
+
+        // AND (&&) OR (||) NOT (!)
+        int idadeBase = 30;
+        double salarioBase = 10000;
+        int idade = 32;
+        double salario = 15000d;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= idadeBase && salario >= salarioBase;
+        boolean isDentroDaLeiMenorQueTrinta = idade <=  idadeBase && salario <= salarioBase;
+
+        System.out.println("Está dentro da lei maior que " + idadeBase + " anos e salario maior que R$" + salarioBase + ": " + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("Está dentro da lei menor que " + idadeBase + " anos e salario menor de R$" + salarioBase + ": " + isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000;
+
+        boolean isPlaystation5Compravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+        System.out.println("Playstation é compravel: " + isPlaystation5Compravel);
     }
 }
