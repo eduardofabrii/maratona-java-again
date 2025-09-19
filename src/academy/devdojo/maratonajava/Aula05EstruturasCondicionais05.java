5d67236cb6278a8be939b4364cbce1ce70b4c09f
eduardo.fabri <eduardo.fabri@wises.com.br>
2025-09-18 23:16:48 -0300

    Video 25 e 26 - Switch Statement e exercicio

---
+++ src/academy/devdojo/maratonajava/Aula05EstruturasCondicionais05.java
@@ -0,0 +1,55 @@
+package academy.devdojo.maratonajava;
+
+public class Aula05EstruturasCondicionais05 {
+    public static void main(String[] args) {
+        // Imprima o dia da semana considerando 1 como domingo
+
+        byte dia = 3;
+        String diaSemana = "";
+
+        // Switch aceita char, int, byte, short, enum e String
+        switch (dia) {
+            case 1:
+                diaSemana = "Domingo";
+                break;
+            case 2:
+                diaSemana = "Segunda";
+                break;
+            case 3:
+                diaSemana = "Terca";
+                break;
+            case 4:
+                diaSemana = "Quarta";
+                break;
+            case 5:
+                diaSemana = "Quinta";
+                break;
+            case 6:
+                diaSemana = "Sexta";
+                break;
+            case 7:
+                diaSemana = "Sabado";
+                break;
+            default:
+                System.out.println("Escolha inválido");
+                break;
+        }
+
+        System.out.println(diaSemana);
+
+        char sexo =  'M';
+        switch (sexo) {
+            case 'M':
+                System.out.println("Homem");
+                break;
+            case 'F':
+                System.out.println("Mulher");
+                break;
+
+            default:
+                System.out.println("Inválido");
+                break;
+        }
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/Aula05EstruturasCondicionais06.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava;
+
+public class Aula05EstruturasCondicionais06 {
+    public static void main(String[] args) {
+        // Dado os valores de 1 a 7, imprima se é dia util ou final de semana
+        // Considerando 1 como domingo
+
+        int dia = 2;
+        boolean isFinalDeSemana = false;
+
+        switch (dia) {
+            case 1, 7 -> isFinalDeSemana = true;
+            case 2, 3, 4, 5, 6 -> isFinalDeSemana = false;
+            default -> System.out.println("Opção inválida");
+        }
+
+        if (isFinalDeSemana) {
+            System.out.println("Final de semana");
+        } else {
+            System.out.println("Dia útil");
+        }
+    }
+}
