126bae0c0f2d512852490b5745a5b0222274aa4f
eduardo.fabri <eduardo.fabri@wises.com.br>
2025-09-23 08:34:40 -0300

    Video 29 e 30 - Break

---
+++ src/academy/devdojo/maratonajava/Aula06EstruturasDeRepeticao04.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava;
+
+public class Aula06EstruturasDeRepeticao04 {
+    public static void main(String[] args) {
+        // Dado um valor de um carro, descubra em quantas vezes ele pode ser parcelado
+        // Condicao valorParcela >= 1000
+
+        float valorCarro = 35341f;
+        float parcelaMinima = 1000f;
+
+        for (int parcela = 1; parcela <= valorCarro; parcela++) {
+            double valorParcela = valorCarro / parcela;
+            if (valorParcela <= parcelaMinima) {
+                break;
+            }
+            System.out.println("Parcela " + parcela + " | R$" + valorParcela);
+        }
+    }
+}
