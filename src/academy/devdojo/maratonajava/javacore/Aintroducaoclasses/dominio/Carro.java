3e880e85b012a770a3220e3e1a2643029a12e2b2
eduardo.fabri <eduardo.fabri@wises.com.br>
2025-10-02 18:37:08 -0300

    Video 42 - Exercicio

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Carro.java
@@ -0,0 +1,45 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
+
+public class Carro {
+    private String nome;
+    private String modelo;
+    private int ano;
+
+    public Carro(String nome, String modelo, int ano) {
+        this.nome = nome;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro | " +
+                "nome: " + nome +
+                ", modelo: " + modelo +
+                ", ano: " + ano;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro("Porsche", "Cayenne", 2025);
+        Carro carro2 = new Carro("Gol", "G3", 2007);
+
+        System.out.println(carro1.toString());
+        System.out.println(carro2.toString());
+    }
+}
